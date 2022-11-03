8de81f4165d1f42f07fbba93c29f97fe541990cc
Isaac0-dev <62234577+Isaac0-dev@users.noreply.github.com>
2022-11-03 16:48:04 +1000

    use my repo to download everything

--- coop-compiler/RepoUtil.cs
+++ coop-compiler/RepoUtil.cs
@@ -37,13 +37,13 @@ namespace coop_builder
         public static string tmpEnvironment32 = "coop-compiler-32-bit";
         public static string tmpEnvironment64 = "coop-compiler-64-bit";
 
-        public static string environmentUrl32 = "https://github.com/coop-compiler/coop-compiler/archive/refs/heads/32-bit.zip";
-        public static string environmentUrl64 = "https://github.com/coop-compiler/coop-compiler/archive/refs/heads/64-bit.zip";
+        public static string environmentUrl32 = "https://github.com/Isaac0-dev/coop-compiler/archive/refs/heads/32-bit.zip";
+        public static string environmentUrl64 = "https://github.com/Isaac0-dev/coop-compiler/archive/refs/heads/64-bit.zip";
 
-        public static string intelHdFix32     = "https://github.com/coop-compiler/coop-compiler/raw/32-bit/intel-hd/opengl32.dll";
-        public static string intelHdFix64     = "https://github.com/coop-compiler/coop-compiler/raw/64-bit/intel-hd/opengl32.dll";
+        public static string intelHdFix32     = "https://github.com/Isaac0-dev/coop-compiler/raw/32-bit/intel-hd/opengl32.dll";
+        public static string intelHdFix64     = "https://github.com/Isaac0-dev/coop-compiler/raw/64-bit/intel-hd/opengl32.dll";
 
-        public static string releaseApiUrl   = "https://api.github.com/repos/coop-compiler/coop-compiler/releases/latest";
-        public static string releaseExeUrl   = "https://github.com/coop-compiler/coop-compiler/releases/latest/download/coop-compiler.exe";
+        public static string releaseApiUrl   = "https://api.github.com/repos/Isaac0-dev/coop-compiler/releases/latest";
+        public static string releaseExeUrl   = "https://github.com/Isaac0-dev/coop-compiler/releases/latest/download/coop-compiler.exe";
     }
 }
